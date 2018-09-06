222e9af6588353f0040c13ec4d4f093e9a0c9d92
Artem Palamarchuk <artembo2020@gmail.com>
2018-09-07 01:59:14 +0300

    net-manager design: refinement

--- frontend/views/net-manager/index.php
+++ frontend/views/net-manager/index.php
@@ -27,21 +27,25 @@ $gd_machine = array();
 </b>
 <p>
 <br>
+<div class = "company-summary-block">
     <?php echo Html::img('@web/storage/logos/' . $model->img . ''); ?><br>
-    Название: <?= $model->name; ?><br>
-    Адрес: <?= $model->address; ?><br>
-    Описание: <?= $model->description; ?><br>
-    Сайт: <?= $model->website; ?><br/>
+    <font size="4">Название:</font>&#8195;<?= $model->name; ?><br>
+    <font size="4">Адрес:</font>&#8195;<?= $model->address; ?><br>
+    <font size="4">Описание:</font>&#8195;<?= $model->description; ?><br>
+    <font size="4">Сайт:</font>&#8195;<?= $model->website; ?><br/>
     <?= Html::a("[".Yii::t('frontend', 'Update')."]", ['company/update'], ['style' => 'background-color: #5c87b2; color: aliceblue']) ?>
+</div>
 </p>
 <p>
 <br>
+<div class = "company-summary-block">
     <b><u>Технічні дані</u></b>
-    <div>Кількість балансоутримувачив: <b><?= $model->getCountBalanceHolder() ?></b></div>
-    <div>Кількість адрес: <b><?= $model->getCountAddress() ?></b></div>
-    <div>Кількість IMEI <b><?= $model->getCountImei() ?></b></div>
-    <div>Кількість ПМ <b><?= $model->getCountWmMashine() ?></b></div>
-    <div>Кількість Дозаторів геля <b><?= $model->getCountGdMashine(); ?></b></div>
+    <div><font size="4">Кількість балансоутримувачив:</font>&#8195;<b><?= $model->getCountBalanceHolder() ?></b></div>
+    <div><font size="4">Кількість адрес:</font>&#8195;<b><?= $model->getCountAddress() ?></b></div>
+    <div><font size="4">Кількість IMEI:</font>&#8195;<b><?= $model->getCountImei() ?></b></div>
+    <div><font size="4">Кількість ПМ:</font>&#8195;<b><?= $model->getCountWmMashine() ?></b></div>
+    <div><font size="4">Кількість Дозаторів геля:</font>&#8195;<b><?= $model->getCountGdMashine(); ?></b></div>
+</div>    
 </p>
 <p>
     <b><u>Фінансові дані</u></b>
