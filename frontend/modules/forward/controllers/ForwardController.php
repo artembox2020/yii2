689b92967dac660cf070ea8186a582ae4d326f38
primemix <monstrpro@gmail.com>
2019-02-06 09:51:52 +0200

    fix forward response

--- frontend/modules/forward/controllers/ForwardController.php
+++ frontend/modules/forward/controllers/ForwardController.php
@@ -5,35 +5,38 @@ namespace frontend\modules\forward\controllers;
 use frontend\models\AddressBalanceHolder;
 use frontend\models\Imei;
 use frontend\models\WmMashine;
-use frontend\services\custom\Debugger;
 use yii\web\Controller;
 
 /**
+ * forward/forward/index?address_name=short address name
+ *
  * Class ForwardController
  * @package frontend\modules\forward\controllers
  */
 class ForwardController extends Controller
 {
     /**
-     * /forward/forward/index?address_name=short address name
-     * 
      * @param $address_name
      * @return \yii\web\Response
      */
     public function actionIndex($address_name)
     {
         $array = array();
-        $address = AddressBalanceHolder::findOne(['name' => $address_name]);
-        $imei = Imei::find(['address_id' => $address->id])
+
+        $address = AddressBalanceHolder::find()->where(['name' => $address_name])->one();
+        $imei = Imei::find()->where(['address_id' => $address->id])
             ->andWhere(['imei.status' => Imei::STATUS_ACTIVE])->one();
-        $wm_machine = WmMashine::find(['imei_id' => $imei->id])
+        $wm_machine = WmMashine::find()->where(['imei_id' => $imei->id])
             ->andWhere(['wm_mashine.status' => WmMashine::STATUS_ACTIVE])->all();
 
         foreach ($wm_machine as $key => $value) {
-            $array[$value->number_device] = ['display' => $value->display, 'status' => $value->current_status];
+            $array[$value->number_device] = [
+                'id' => $value->id,
+                'display' => $value->display,
+                'status' => $value->current_status];
         }
-//        Debugger::dd($array);
-        
+
         return $this->asJson($array);
+
     }
 }
