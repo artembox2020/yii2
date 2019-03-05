33736cf4c15c97679c39491a981794fe29f7e37d
primemix <monstrpro@gmail.com>
2019-03-05 17:56:04 +0200

    changed smtp server

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -545,97 +545,97 @@ class SiteController extends Controller
         }
     }
 
-//    const HOUR = 1;
-//    const FORMAT = 'Y-m-d H:i:s';
-//    const FILE = '../services/error/db_connection/file/data.json';
-//    public $mail = [
-//        'Serhii' => 'monstrpro@gmail.com',
-//        'Dmitro' => 'dmytro.v.kovtun@gmail.com',
-//        'Sasha' => 'sashabardash@gmail.com',
-//        'Info' => 'info@postirayka.com.ua'
-//    ];
-//    /**
-//     * Если разница между ошибками один час то записывает в файл data.json дату ошибки
-//     * и отправляет сообщение на почту
-//     *
-//     * @throws \Exception
-//     */
-//    public function actionTest($message)
-//    {
-//        if ($this->diffHour(date(self::FORMAT), $this->getDateFromFile()) > self::HOUR) {
-//            $this->save();
-//            $this->senderMessages($this->mail, $message);
-//        }
-//    }
-//
-//    public function save()
-//    {
-//        $dateArray = Json::decode(file_get_contents(self::FILE), $asArray = true);
-//        $dateArray[] = ['date' => date(self::FORMAT)];
-//        file_put_contents(self::FILE, Json::encode($dateArray));
-//
-//        unset($dateArray);
-//
-//        echo 'save' . '<br>';
-//    }
-//
-//    /**
-//     * Вернёт последнюю дату ошибки из файла data.json
-//     * Если массива нет (пустой файл) создаст массив и запишет дату первой ошибки
-//     * @return mixed
-//     */
-//    public function getDateFromFile()
-//    {
-//        if (!$this->getArrayDate()) {
-//            $this->save();
-//        }
-//
-//        $array = $this->getArrayDate();
-//        $value = array_pop($array);
-//
-//        return $value->date;
-//    }
-//
-//    /**
-//     * Вернёт разницу между двумя датами, число - в часах
-//     *
-//     * @param $current_time
-//     * @param $time_from_file
-//     * @return int
-//     * @throws \Exception
-//     */
-//    public function diffHour($current_time, $time_from_file)
-//    {
-//        $currentDate = new DateTime($current_time);
-//
-//        return $currentDate->diff(new DateTime($time_from_file))->h;
-//    }
-//
-//    /**
-//     * Вернёт массив дат (дата "ошибки" исключения базы данных)
-//     * @return array
-//     */
-//    public function getArrayDate()
-//    {
-//        return json_decode(file_get_contents(self::FILE));
-//    }
-//
-//    public function pushMessage($mail, $name, $message)
-//    {
-//        Yii::$app->mailer->compose('db-error', [
-//            'textBody' => $message,
-//        ])
-//            ->setFrom('sense.servers@gmail.com')
-//            ->setTo($mail)
-//            ->setSubject('Hello, ' . $name . '!')
-//            ->setTextBody('')
-//            ->send();
-//    }
-//
-//    public function senderMessages($mails, $message)
-//    {
-//        foreach ($mails as $name => $email) {
-//            $this->pushMessage($email, $name, $message);
-//        }
-//    }
+    const HOUR = 1;
+    const FORMAT = 'Y-m-d H:i:s';
+    const FILE = '../services/error/db_connection/file/data.json';
+    public $mail = [
+        'Serhii' => 'monstrpro@gmail.com',
+        'Dmitro' => 'dmytro.v.kovtun@gmail.com',
+        'Sasha' => 'sashabardash@gmail.com',
+        'Info' => 'info@postirayka.com.ua'
+    ];
+    /**
+     * Если разница между ошибками один час то записывает в файл data.json дату ошибки
+     * и отправляет сообщение на почту
+     *
+     * @throws \Exception
+     */
+    public function actionTest($message)
+    {
+        if ($this->diffHour(date(self::FORMAT), $this->getDateFromFile()) > self::HOUR) {
+            $this->save();
+            $this->senderMessages($this->mail, $message);
+        }
+    }
+
+    public function save()
+    {
+        $dateArray = Json::decode(file_get_contents(self::FILE), $asArray = true);
+        $dateArray[] = ['date' => date(self::FORMAT)];
+        file_put_contents(self::FILE, Json::encode($dateArray));
+
+        unset($dateArray);
+
+        echo 'save' . '<br>';
+    }
+
+    /**
+     * Вернёт последнюю дату ошибки из файла data.json
+     * Если массива нет (пустой файл) создаст массив и запишет дату первой ошибки
+     * @return mixed
+     */
+    public function getDateFromFile()
+    {
+        if (!$this->getArrayDate()) {
+            $this->save();
+        }
+
+        $array = $this->getArrayDate();
+        $value = array_pop($array);
+
+        return $value->date;
+    }
+
+    /**
+     * Вернёт разницу между двумя датами, число - в часах
+     *
+     * @param $current_time
+     * @param $time_from_file
+     * @return int
+     * @throws \Exception
+     */
+    public function diffHour($current_time, $time_from_file)
+    {
+        $currentDate = new DateTime($current_time);
+
+        return $currentDate->diff(new DateTime($time_from_file))->h;
+    }
+
+    /**
+     * Вернёт массив дат (дата "ошибки" исключения базы данных)
+     * @return array
+     */
+    public function getArrayDate()
+    {
+        return json_decode(file_get_contents(self::FILE));
+    }
+
+    public function pushMessage($mail, $name, $message)
+    {
+        Yii::$app->mailer->compose('db-error', [
+            'textBody' => $message,
+        ])
+            ->setFrom('sense.servers@gmail.com')
+            ->setTo($mail)
+            ->setSubject('Hello, ' . $name . '!')
+            ->setTextBody('')
+            ->send();
+    }
+
+    public function senderMessages($mails, $message)
+    {
+        foreach ($mails as $name => $email) {
+            $this->pushMessage($email, $name, $message);
+        }
+    }
 }
