a204e89e4f0baa6cd5526054ed696b38b12aa1c0
primemix <monstrpro@gmail.com>
2019-04-05 17:48:01 +0300

    my middle

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,19 +1,19 @@
 <?php
 
-define('YII_ENV', 'test');
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-require __DIR__ .'/../vendor/autoload.php';
+//define('YII_ENV', 'test');
+//defined('YII_DEBUG') or define('YII_DEBUG', true);
+//require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+//require __DIR__ .'/../vendor/autoload.php';
 /**
  * Setting path aliases
  */
-Yii::setAlias('root', realpath(__DIR__ . '/../../'));
-Yii::setAlias('common', realpath(__DIR__ . '/../../common'));
-Yii::setAlias('frontend', realpath(__DIR__ . '/../../frontend'));
-Yii::setAlias('backend', realpath(__DIR__ . '/../../backend'));
-Yii::setAlias('console', realpath(__DIR__ . '/../../console'));
-Yii::setAlias('storage', realpath(__DIR__ . '/../../storage'));
-Yii::setAlias('test', realpath(__DIR__ . '/../../test'));
+//Yii::setAlias('root', realpath(__DIR__ . '/../../'));
+//Yii::setAlias('common', realpath(__DIR__ . '/../../common'));
+//Yii::setAlias('frontend', realpath(__DIR__ . '/../../frontend'));
+//Yii::setAlias('backend', realpath(__DIR__ . '/../../backend'));
+//Yii::setAlias('console', realpath(__DIR__ . '/../../console'));
+//Yii::setAlias('storage', realpath(__DIR__ . '/../../storage'));
+//Yii::setAlias('test', realpath(__DIR__ . '/../../tests'));
 
 /**
  * Setting url aliases
