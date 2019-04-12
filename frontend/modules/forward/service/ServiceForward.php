f26c0a9311aa4ced6a65acc119a9bbf92d167055
Prime Mix <monstrpro@gmail.com>
2019-04-12 22:44:08 +0300

    add not found exception for forward service

--- frontend/modules/forward/service/ServiceForward.php
+++ frontend/modules/forward/service/ServiceForward.php
@@ -9,6 +9,7 @@ use frontend\models\ImeiData;
 use frontend\models\WmMashine;
 use frontend\modules\forward\interfaces\ServiceForwardInterface;
 use frontend\services\custom\Debugger;
+use http\Exception;
 use Yii;
 use yii\db\Query;
 
@@ -22,14 +23,16 @@ class ServiceForward implements ServiceForwardInterface
     public $array = array();
     public $result = array();
 
+
     /**
+     *
      * getStaff принимает строку (короткий адрес) возвращает связанные объекты в сформированном
      * массиве
      * array(3) {
     ["param"]=>array(2) {
-       ["central_board_status"]=>string(2) "OK"
-       ["bill_acceptor_status"]=>string(2) "OK"
-        }
+    ["central_board_status"]=>string(2) "OK"
+    ["bill_acceptor_status"]=>string(2) "OK"
+    }
     ["BalanceHolder"]=>string(8) "КІМО"
     ["вул. Мельникова 36/1 5"]=>array(2) {
     [1]=>array(4) {
@@ -43,55 +46,59 @@ class ServiceForward implements ServiceForwardInterface
     ["display"]=>string(2) "  "
     ["date"]=>string(16) "12:31 12.04.2019"
     ["status"]=>string(32) "Готовий до роботи"
-            }
-        }
     }
-    ...
+    }
+    }
      * @param string $address_name
-     * @return array
+     * @return array | mixed
+     * @throws \yii\db\Exception
      */
     public function getStaff(string $address_name): array
     {
-        $address = AddressBalanceHolder::find()
-            ->andWhere(['name' => $address_name])
-            ->one();
+            $address = AddressBalanceHolder::find()
+                ->andWhere(['name' => $address_name])
+                ->one();
 
-        $balanceHolderName = $address->balanceHolder->name;
+            $balanceHolderName = $address->balanceHolder->name;
 
-        $imei = Imei::find()
-            ->andWhere(['address_id' => $address->id])
-            ->andWhere(['imei.status' => Imei::STATUS_ACTIVE])
-            ->one();
+            $imei = Imei::find()
+                ->andWhere(['address_id' => $address->id])
+                ->andWhere(['imei.status' => Imei::STATUS_ACTIVE])
+                ->one();
 
-        $wm_machine = WmMashine::find()
-            ->andWhere(['imei_id' => $imei->id])
-            ->andWhere(['wm_mashine.status' => WmMashine::STATUS_ACTIVE])
-            ->all();
+            $wm_machine = WmMashine::find()
+                ->andWhere(['imei_id' => $imei->id])
+                ->andWhere(['wm_mashine.status' => WmMashine::STATUS_ACTIVE])
+                ->all();
 
-        $res = Yii::$app->db->createCommand('SELECT *
+            $res = Yii::$app->db->createCommand('SELECT *
               FROM imei_data WHERE imei_id = :imei_id ORDER BY created_at DESC LIMIT 1')
-            ->bindValue(':imei_id', $imei->id)
-            ->queryOne();
+                ->bindValue(':imei_id', $imei->id)
+                ->queryOne();
 
-        $imeiData = new ImeiData();
+            $imeiData = new ImeiData();
 
 
-        $this->result['param'] = [
-            'central_board_status' => Yii::t('imeiData', $imeiData->status_central_board[$res['packet']]),
-            'bill_acceptor_status' => Yii::t('imeiData', $imeiData->status_bill_acceptor[$res['evt_bill_validator']])
-        ];
+            $this->result['param'] = [
+                'central_board_status' => Yii::t('imeiData', $imeiData->status_central_board[$res['packet']]),
+                'bill_acceptor_status' => Yii::t('imeiData', $imeiData->status_bill_acceptor[$res['evt_bill_validator']])
+            ];
 
-        $this->result['BalanceHolder'] = $balanceHolderName;
+            $this->result['BalanceHolder'] = $balanceHolderName;
 
-        foreach ($wm_machine as $key => $value) {
-            $this->array[$value->number_device] = [
-                'device_number' => $value->number_device,
-                'display' => $value->display,
-                'date' => date(self::DATE_FORMAT, $value->ping),
-                'status' => Yii::t('frontend', $value->current_state[$value->current_status])];
-        }
+            foreach ($wm_machine as $key => $value) {
+                $this->array[$value->number_device] = [
+                    'device_number' => $value->number_device,
+                    'display' => $value->display,
+                    'date' => date(self::DATE_FORMAT, $value->ping),
+                    'status' => Yii::t('frontend', $value->current_state[$value->current_status])];
+            }
 
-        $this->result[$address->address . ' ' . $address->floor] = $this->array;
+            $this->result[$address->address . ' ' . $address->floor] = $this->array;
+
+            if (!$address) {
+                $this->result = ['address' => Yii::t('common', 'Not found')];
+            }
 
         return $this->result;
     }
