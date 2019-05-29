e3c620f5f7c39afe7039e82c17eb04eac5f214f7
Maxim <m.olefirenko@gmail.com>
2019-05-29 21:46:46 +0300

    Добавлено описание и сделано форматирование psr

--- console/controllers/RbacFlushController.php
+++ console/controllers/RbacFlushController.php
@@ -9,6 +9,12 @@ use common\models\User;
 
 class RbacFlushController extends Controller
 {
+    /**
+     * Сброс всех привелегий и создание привелегий и ролей по умолчанию
+     * Присвоение роли Super Administrator пользователю с id = 1
+     * Run command: yii rbac-flush/init
+     * @throws \yii\base\Exception
+     */
     public function actionInit()
     {
         $auth = Yii::$app->authManager;
@@ -52,29 +58,29 @@ class RbacFlushController extends Controller
         $manager = $auth->createRole(User::ROLE_FINANCIER);
         $manager->description = 'Financier';
         $auth->add($manager);
-        $auth->addChild($manager,$viewFinData);
-        $auth->addChild($manager,$editFinData);
+        $auth->addChild($manager, $viewFinData);
+        $auth->addChild($manager, $editFinData);
 
         $tech = $auth->createRole(User::ROLE_TECHNICIAN);
         $tech->description = 'Technician';
         $auth->add($tech);
-        $auth->addChild($tech,$viewTechData);
-        $auth->addChild($tech,$editTechData);
+        $auth->addChild($tech, $viewTechData);
+        $auth->addChild($tech, $editTechData);
 
         $admin = $auth->createRole(User::ROLE_ADMINISTRATOR);
         $admin->description = 'Administrator';
         $auth->add($admin);
-        $auth->addChild($admin,$viewCompanyData);
-        $auth->addChild($admin,$editCompanyData);
-        $auth->addChild($admin,$manager);
-        $auth->addChild($admin,$tech);
+        $auth->addChild($admin, $viewCompanyData);
+        $auth->addChild($admin, $editCompanyData);
+        $auth->addChild($admin, $manager);
+        $auth->addChild($admin, $tech);
 
         $super_admin = $auth->createRole(User::ROLE_SUPER_ADMINISTRATOR);
         $super_admin->description = 'Super Administrator';
         $auth->add($super_admin);
-        $auth->addChild($super_admin,$loginToBackend);
-        $auth->addChild($super_admin,$changeCompany);
-        $auth->addChild($super_admin,$admin);
+        $auth->addChild($super_admin, $loginToBackend);
+        $auth->addChild($super_admin, $changeCompany);
+        $auth->addChild($super_admin, $admin);
 
         $auth->assign($super_admin, 1);
 
