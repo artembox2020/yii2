71c389aa0d64bb5c00df8bb2797de3e76eda5bb2
Maxim <m.olefirenko@gmail.com>
2019-07-02 18:47:18 +0300

    добавлена миграция для создания внешних ключей в таблице customer_cards

--- console/migrations/m190629_124604_add_columns_company_user_to_customer_card_table.php
+++ console/migrations/m190629_124604_add_columns_company_user_to_customer_card_table.php
@@ -5,7 +5,7 @@ use yii\db\Migration;
 /**
  * Class m190629_124604_add_column_company_to_customer_card_table
  */
-class m190629_124604_add_column_company_to_customer_card_table extends Migration
+class m190629_124604_add_columns_company_user_to_customer_card_table extends Migration
 {
     public function up()
     {
@@ -26,6 +26,23 @@ class m190629_124604_add_column_company_to_customer_card_table extends Migration
             'company',
             'id'
         );
+        $this->addColumn(
+            'customer_cards',
+            'user_id',
+            $this->integer()
+        );
+        $this->createIndex(
+            'idx-customer_cards-user_id',
+            'customer_cards',
+            'user_id'
+        );
+        $this->addForeignKey(
+            'fk-customer_cards-user_id',
+            'customer_cards',
+            'user_id',
+            'user',
+            'id'
+        );
     }
 
     public function down()
@@ -41,6 +58,18 @@ class m190629_124604_add_column_company_to_customer_card_table extends Migration
         $this->dropColumn(
             'customer_cards',
             'company_id'
+        );
+        $this->dropForeignKey(
+            'fk-customer_cards-user_id',
+            'customer_cards'
+        );
+        $this->dropIndex(
+            'idx-customer_cards-user_id',
+            'customer_cards'
+        );
+        $this->dropColumn(
+            'customer_cards',
+            'user_id'
         );
     }
 }
