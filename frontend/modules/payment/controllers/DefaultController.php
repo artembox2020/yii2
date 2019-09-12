1dd3634b65009614273316a5251051a3c3ccdae7
Artem Palamarchuk <artembo2020@gmail.com>
2019-09-12 10:16:19 +0300

    card: fix payment

--- frontend/modules/payment/controllers/DefaultController.php
+++ frontend/modules/payment/controllers/DefaultController.php
@@ -44,7 +44,12 @@ class DefaultController extends Controller
     {
         $model = new DynamicModel(['card_no','amount']);
 
-        $cards = CustomerCards::find(['is_deleted' => self::FALSE])->select('card_no')->asArray()->column();
+        $cards = CustomerCards::find()
+        ->andWhere([
+            'is_deleted' => self::FALSE,
+            'status' => CustomerCards::STATUS_ACTIVE
+        ])
+        ->select('card_no')->asArray()->column();
 
         $model
             ->addRule(['card_no', 'amount'],  'required')
