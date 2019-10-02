cf1dad986782010578b643081606fb1ae376897e
Prime Mix <monstrpro@gmail.com>
2019-10-02 13:01:05 +0300

    add try catch to command json

--- api/modules/v2d00/UseCase/Command/Command.php
+++ api/modules/v2d00/UseCase/Command/Command.php
@@ -6,24 +6,35 @@ use Yii;
 
 class Command
 {
+    const CONST_FALSE = 0;
+    const CONST_TRUE = 1;
+
     public static function getCommand(int $imei)
     {
-        $rows = (new \yii\db\Query())
-            ->select(['action', 'unix_time_offset'])
-            ->from('imei_action')
-            ->where(['imei' => $imei])
-            ->andWhere(['is_active' => 1])
-            ->limit(1)
-            ->one();
+        try {
+            $rows = (new \yii\db\Query())
+                ->select(['action', 'unix_time_offset'])
+                ->from('imei_action')
+                ->where(['imei' => $imei])
+                ->andWhere(['is_active' => self::CONST_TRUE])
+                ->limit(self::CONST_TRUE)
+                ->one();
 
-        $returnData = ['time' => $rows['unix_time_offset'], 'cmd' => $rows['action']];
-        $response = Yii::$app->response;
-        $response->format = \yii\web\Response::FORMAT_JSON;
-        $response->data = $returnData;
+            $returnData = ['time' => $rows['unix_time_offset'], 'cmd' => $rows['action']];
+            $response = Yii::$app->response;
+            $response->format = \yii\web\Response::FORMAT_JSON;
+            $response->data = $returnData;
 
-        Yii::$app->db->createCommand()
-            ->update('imei_action', ['is_active' => 0], ['imei' => $imei, 'is_active' => 1])
-            ->execute();
+            Yii::$app->db->createCommand()
+                ->update('imei_action',
+                    ['is_active' => self::CONST_FALSE],
+                    ['imei' => $imei, 'is_active' => self::CONST_TRUE])
+                ->execute();
+        } catch (\Exception $exception) {
+            Yii::$app->response->statusCode = 500;
+
+            return $exception;
+        }
 
         return $response;
     }
