4cb034ff824c0ed1ff7e3471c69a564888a926dc
Artem Palamarchuk <artembo2020@gmail.com>
2019-10-07 16:27:29 +0300

    logs: little fix

--- frontend/models/CbLogSearch.php
+++ frontend/models/CbLogSearch.php
@@ -19,7 +19,7 @@ class CbLogSearch extends CbLog
     const PAGE_SIZE = 10;
     const TYPE_ENCASHMENT_STATUS = 8;
     const TYPE_LAST_ENCASHMENT_DATE = '01.01.2019';
-    const TYPE_ITEMS_LIMIT = 20000;
+    const TYPE_ITEMS_LIMIT = 500;
 
     public $address;
 
