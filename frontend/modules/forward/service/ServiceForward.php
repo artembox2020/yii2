c4b719d1dc6b3f2e6277fe16a7d57454d0bf976b
Prime Mix <monstrpro@gmail.com>
2019-10-29 13:55:36 +0200

    add +1 hourse forward module

--- frontend/modules/forward/service/ServiceForward.php
+++ frontend/modules/forward/service/ServiceForward.php
@@ -97,7 +97,7 @@ class ServiceForward implements ServiceForwardInterface
             $this->array[$value->number_device] = [
                 'device_number' => $value->number_device,
                 'display' => $value->display,
-                'date' => date(self::DATE_FORMAT, $value->ping),
+                'date' => date(self::DATE_FORMAT, $value->ping + 60*60),//добавил один час ("+60*60" переход на зименее)
                 'status' => Yii::t('frontend', $value->current_state[$value->current_status])
             ];
         }
